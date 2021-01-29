34a3af3ffec248d746a123545aa838cc7f1f7d25
MOEKASAKAI <wpi.floves2@gmail.com>
2021-01-29 01:38:03 +0000

    [Add] 4-4 コメント投稿・削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'post_images#index'
+  root 'post_images#index'
   devise_for :users
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
   
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 end
